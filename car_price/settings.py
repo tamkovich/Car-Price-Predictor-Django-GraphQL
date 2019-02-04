697b82ea44ea2e9f94ced9b9c44da4e17276eb86
Denis Tamkovich <jaselnikpromise@gmail.com>
2019-02-04 19:25:22 +0300

    initialize graphene

--- car_price/settings.py
+++ car_price/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'graphene_django',
+
     'predictor',
 ]
 
@@ -124,3 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+GRAPHENE = {
+    'SCHEMA': 'api.schema.schema'
+}
