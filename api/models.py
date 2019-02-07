f505144c9eb734ca20a61a86c55401f468eb94a9
Denis Tamkovich <jaselnikpromise@gmail.com>
2019-02-07 14:15:15 +0300

    dataset to sql

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    """
+    All: ['price', 'year_model', 'mileage', 'fuel_type', 'mark',
+    'model', 'fiscal_power', 'sector', 'type', 'city']
+    Less: ['price', 'year_model', 'mileage',
+    'fiscal_power', 'fuel_type', 'mark']
+    """
+    price = models.PositiveIntegerField(blank=True)  # label to predict
+
+    year_model = models.PositiveIntegerField()
+    mileage = models.PositiveIntegerField()
+    fiscal_power = models.PositiveIntegerField()
+    fuel_type = models.CharField(max_length=20)
+    mark = models.CharField(max_length=30)
--- car_price/settings.py
+++ car_price/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'graphene_django',
 
     'predictor',
+    'api',
 ]
 
 MIDDLEWARE = [
