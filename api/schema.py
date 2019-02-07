5e84ca268972f74068741b3af9a554d3cefd031c
Denis Tamkovich <jaselnikpromise@gmail.com>
2019-02-07 14:37:49 +0300

    query filter via id and mark

--- api/schema.py
+++ api/schema.py
@@ -13,5 +13,23 @@ class CarType(DjangoObjectType):
 class Query(object):
     all_cars = graphene.List(CarType)
 
+    car = graphene.Field(
+        CarType,
+        id=graphene.Int(),
+        mark=graphene.String()
+    )
+
     def resolve_all_cars(self, info, **kwargs):
         return Car.objects.all()[:10]
+
+    def resolve_car(self, info, **kwargs):
+        id = kwargs.get('id')
+        mark = kwargs.get('mark')
+
+        if id is not None:
+            return Car.objects.get(pk=id)
+
+        if mark is not None:
+            return Car.objects.filter(mark=mark).first()
+
+        return None
